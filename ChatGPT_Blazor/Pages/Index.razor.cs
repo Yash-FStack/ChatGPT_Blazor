d3a5794af3a9c234e2e783f034ef78316b0889c9
Yash Kumar Rajora <78166916+yashkumarajora@users.noreply.github.com>
2023-02-10 12:10:26 +0530

    Initial Commit

---
+++ ChatGPT_Blazor/Pages/Index.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
+using System;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using System.Net.Http.Headers;
+using OpenAI_API;
+using System.Threading.Channels;
+
+namespace ChatGPT_Blazor.Pages
+{
+    public partial class Index:ComponentBase
+    {
+        public string message { get; set; }
+        public string generatedText { get; set; } = "Answer will be displayed here.";
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        private async Task GetResponseFromGPT3()
+        {
+            generatedText = "Finding Answer...";
+            try
+            {
+                string apiKey = "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";
+                string answer = string.Empty;
+                var openai = new OpenAIAPI(apiKey);
+                CompletionRequest completion = new CompletionRequest();
+                completion.Prompt = message;
+                completion.Model = OpenAI_API.Model.DavinciText;
+                completion.MaxTokens = 4000;
+                var result = await openai.Completions.CreateCompletionAsync(completion);
+
+                if (result != null)
+                {
+                    foreach (var item in result.Completions)
+                    {
+                        GPT3Response.Response = item.Text;
+                    }
+                    generatedText = GPT3Response.Response;
+                }
+
+                StateHasChanged();
+            }
+             catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        
+
+
+        public static class GPT3Response
+        {
+            public static string Response { get; set; }
+        }
+
+    }
+}
